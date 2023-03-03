75215c47a2822563e4414a3aa3644291898b0ffb
wzwolfx <wzwolfxdev@gmail.com>
2023-03-03 18:05:28 +0800

    completed code for supplier-image_upload note to self included code to check for extension jpeg
     Changes to be committed:
    	modified:   supplier_image_upload.py

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -9,14 +9,23 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 url = "http://localhost/upload/"
 dir_name = os.path.join("supplier-data","images")
+extension = ".jpeg"
 
 def upload(file_path,dest_url):
     with open(file_path,'rb') as opened:
         r = requests.post(dest_url, files={'file':opened})
 
 def main():
-    dir_path = ""
+    """upload all the files in dir_name to url api"""
+    
+    dir_path = os.path.abspath(dir_name)
     logging.debug(dir_path)
+    for name in os.listdir(dir_path):
+        fullname = os.path.join(dir_path,name)
+        # note - only upload the jpeg extensions
+        if os.path.isfile(fullname) and os.path.splitext(name)[1] == extension :
+            upload(fullname,url)
+            logging.info("uploaded {} to url".format(name))
 
 if __name__ == '__main__':
     main()
